eccceefed4dd51d38e10651abdcff35caba27966
kirargomedo23 <kirargomedo@gmail.com>
2023-03-02 10:45:34 -0500

    feat: loader

--- src/app/shared/components/loading/loading.module.ts
+++ src/app/shared/components/loading/loading.module.ts
@@ -2,14 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './loading.component';
 
-
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 
 @NgModule({
   declarations: [
     LoadingComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,MatProgressSpinnerModule
   ],
   exports: [LoadingComponent]
 })
