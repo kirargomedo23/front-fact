f1e09057b77e856e34f71aed912b21335b442895
kirargomedo23 <kirargomedo@gmail.com>
2023-03-02 10:32:21 -0500

    feat: estructura login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { LocalStorageService } from '@app/services/localStorage/local-storage.service';
 
@@ -11,15 +11,33 @@ import { LocalStorageService } from '@app/services/localStorage/local-storage.se
 })
 export class LoginComponent implements OnInit {
 
+  formLogin: FormGroup;
+  loading: boolean = false;
+
+
   constructor(
     private readonly formBuilder: FormBuilder,
     private readonly router: Router,
     private readonly localStorageService: LocalStorageService,
   ){
+    this.formLogin = this.formBuilder.group({
+      usuario:[null, [Validators.required]],
+      contraseña:[null, [Validators.required]],
+    });
 
   }
 
+
+
   ngOnInit(): void {
+
+  }
+
+  getFormLogin(control: string) {
+    return this.formLogin.get(control);
+  }
+
+  startSession(){
 
   }
 
