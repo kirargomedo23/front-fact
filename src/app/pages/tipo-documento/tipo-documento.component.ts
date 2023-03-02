137b13219589a3dead8e31f71bd7e699378f67d5
kirargomedo23 <kirargomedo@gmail.com>
2023-03-02 10:00:12 -0500

    feat: estructura para la página de tipo-documento

--- src/app/pages/tipo-documento/tipo-documento.component.ts
+++ src/app/pages/tipo-documento/tipo-documento.component.ts
@@ -1,6 +1,11 @@
+import { MatDialog } from '@angular/material/dialog';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { TipoDocumento } from '@app/interfaces/tipo-documento.interface';
 import { TipoDocumentoService } from '@app/services/tipo-documento/tipo-documento.service';
 import { UtilService } from '@app/services/util/util.service';
+import { ModalConfirmationComponent } from '@app/shared/components/modal-confirmation/modal-confirmation.component';
+import { MatSlideToggleChange } from '@angular/material/slide-toggle';
 
 @Component({
   selector: 'app-tipo-documento',
@@ -10,15 +15,70 @@ import { UtilService } from '@app/services/util/util.service';
 export class TipoDocumentoComponent implements OnInit {
 
   public title: String = 'Actualizar Tipo de Documento';
+  public titleSlideToggle: string = 'Activo';
+
+
+
+  formTipoDocumento: FormGroup;
+
 
   constructor(
+    private readonly matDialog: MatDialog,
+    private readonly formBuilder: FormBuilder,
     private readonly utilService: UtilService,
     private readonly tipoDocumentoService: TipoDocumentoService
   ){
-
+    this.formTipoDocumento = this.formBuilder.group({
+      codigo:[null, [Validators.required]],
+      nombre:[null, [Validators.required]],
+      descripcion:[null],
+      estado:[null, [Validators.required]]
+    });
   }
 
   ngOnInit(): void {
+    this.getDocumento();
+  }
+
+  get getValueFormTipoDocumento(){
+    return this.formTipoDocumento.value;
+  }
+
+  getFormTipoDocumentoControls(control: string) {
+    return this.formTipoDocumento.get(control);
+  }
+
+  getDocumento(){
 
   }
+
+  updateValueForm(data: TipoDocumento){
+    this.getFormTipoDocumentoControls('nombre')?.setValue(data.nombre);
+    this.getFormTipoDocumentoControls('estado')?.setValue(data.estado);
+  }
+
+  openModal(){
+
+    this.matDialog.open(ModalConfirmationComponent, {
+      disableClose: false,
+      width: "400px",
+    }).afterClosed()
+    .subscribe(({answer}) => {
+        if (answer === "yes") {
+          this.update();
+        }
+    });
+
+  }
+
+  update(){
+    console.log("metodo update");
+
+  }
+
+  changeSlide(event:MatSlideToggleChange){
+    this.titleSlideToggle = (event.checked) ? 'Activo': 'Inactivo';
+
+  }
+
 }
--- src/app/pages/tipo-documento/tipo-documento.module.ts
+++ src/app/pages/tipo-documento/tipo-documento.module.ts
@@ -4,14 +4,22 @@ import { CommonModule } from '@angular/common';
 import { TipoDocumentoRoutingModule } from './tipo-documento-routing.module';
 import { TipoDocumentoComponent } from './tipo-documento.component';
 
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+
+
+import { UtilService } from '@app/services/util/util.service';
+import { ModalConfirmationModule } from '@app/shared/components/modal-confirmation/modal-confirmation.module';
+import { FormsAngularMaterialModule } from '@app/shared/modules/forms-angular-material.module';
 
 @NgModule({
   declarations: [
     TipoDocumentoComponent
   ],
   imports: [
-    CommonModule,
-    TipoDocumentoRoutingModule
-  ]
+    CommonModule,  TipoDocumentoRoutingModule,
+    ModalConfirmationModule, FormsAngularMaterialModule,
+    MatSlideToggleModule
+  ],
+  providers:[UtilService]
 })
 export class TipoDocumentoModule { }
