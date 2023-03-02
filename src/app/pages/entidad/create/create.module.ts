a4698e2c24d0246dc83f168d9d9f6d9b009ebecc
kirargomedo23 <kirargomedo@gmail.com>
2023-03-02 10:13:17 -0500

    fix: correción en modulos de entidad

--- src/app/pages/entidad/create/create.module.ts
+++ src/app/pages/entidad/create/create.module.ts
@@ -5,13 +5,20 @@ import { CreateRoutingModule } from './create-routing.module';
 import { CreateComponent } from './create.component';
 
 
+import { UtilService } from '@app/services/util/util.service';
+import { ModalConfirmationModule } from '@app/shared/components/modal-confirmation/modal-confirmation.module';
+import { FormsAngularMaterialModule } from '@app/shared/modules/forms-angular-material.module';
+
+
+
 @NgModule({
   declarations: [
     CreateComponent
   ],
   imports: [
-    CommonModule,
-    CreateRoutingModule
-  ]
+    CommonModule, CreateRoutingModule,
+    ModalConfirmationModule, FormsAngularMaterialModule
+  ],
+  providers:[UtilService]
 })
 export class CreateModule { }
--- src/app/pages/entidad/list/list.module.ts
+++ src/app/pages/entidad/list/list.module.ts
@@ -4,14 +4,21 @@ import { CommonModule } from '@angular/common';
 import { ListRoutingModule } from './list-routing.module';
 import { ListComponent } from './list.component';
 
+import { UtilService } from '@app/services/util/util.service';
+import { ModalConfirmationModule } from '@app/shared/components/modal-confirmation/modal-confirmation.module';
+
+
+import { FormsAngularMaterialModule } from '@app/shared/modules/forms-angular-material.module';
+
 
 @NgModule({
   declarations: [
     ListComponent
   ],
   imports: [
-    CommonModule,
-    ListRoutingModule
-  ]
+    CommonModule,  ListRoutingModule,
+    FormsAngularMaterialModule, ModalConfirmationModule
+  ],
+  providers:[UtilService]
 })
 export class ListModule { }
