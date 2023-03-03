5f29e304cce873ab5dba08c55beb074e93ed4ac3
kirargomedo23 <kirargomedo@gmail.com>
2023-03-03 08:46:02 -0500

    fix: correción en login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { LocalStorageService } from '@app/services/localStorage/local-storage.service';
+import { LocalStorageService } from '@services/localStorage/local-storage.service';
+import { UtilService } from '@services/util/util.service';
 
 
 @Component({
@@ -11,19 +12,17 @@ import { LocalStorageService } from '@app/services/localStorage/local-storage.se
 })
 export class LoginComponent implements OnInit {
 
-  formLogin: FormGroup;
+  formLogin: FormGroup = new FormGroup({});
   loading: boolean = false;
 
 
   constructor(
     private readonly formBuilder: FormBuilder,
     private readonly router: Router,
+    private readonly utilService: UtilService,
     private readonly localStorageService: LocalStorageService,
   ){
-    this.formLogin = this.formBuilder.group({
-      usuario:[null, [Validators.required]],
-      contraseña:[null, [Validators.required]],
-    });
+    this.startFormLogin();
 
   }
 
@@ -33,11 +32,34 @@ export class LoginComponent implements OnInit {
 
   }
 
+  startFormLogin(){
+    this.formLogin = this.formBuilder.group({
+      usuario:[null, [Validators.required]],
+      contrasenia:[null, [Validators.required]],
+    });
+  }
+
   getFormLogin(control: string) {
     return this.formLogin.get(control);
   }
 
   startSession(){
+    const isInvalidForm = this.formLogin.invalid;
+
+    if(isInvalidForm){
+      this.utilService.openMessageError('Formulario inválido. Por favor, verifique.');
+      return;
+    }
+
+    const usuario = this.getFormLogin('usuario')?.value;
+    const contrasenia = this.getFormLogin('contrasenia')?.value;
+
+    if(usuario !== 'master' || contrasenia !== 'master'){
+      this.utilService.openMessageError('Credenciales inválidas');
+      return;
+    }
+
+    this.navigateToPageEntidad();
 
   }
 
--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,3 +1,4 @@
+import { UtilService } from '@services/util/util.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -13,6 +14,7 @@ import { FormsAngularMaterialModule } from '@app/shared/modules/forms-angular-ma
   imports: [
     CommonModule, LoginRoutingModule,
     FormsAngularMaterialModule,
-  ]
+  ],
+  providers:[UtilService]
 })
 export class LoginModule { }
